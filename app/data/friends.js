4d9079c238e647273d62f9adf11abb9920d00a20
Reuben Taveras <rtaveras824@hotmail.com>
2016-09-23 23:54:33 -0400

    Changed question values to a score array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,18 @@
 var friends = [
 	{
 		"name": "Yuuu",
-		"q1": "1",
-		"q2": "1",
-		"q3": "1",
-		"q4": "1",
-		"q5": "1"
+		"photo": "www.google.com",
+		"scores": [1, 1, 1, 1, 1]
 	},
 	{
 		"name": "Reuben",
-		"q1": "2",
-		"q2": "2",
-		"q3": "2",
-		"q4": "2",
-		"q5": "2"
+		"photo": "www.google.com",
+		"scores": [2, 2, 2, 2, 2]
 	},
 	{
 		"name": "Babe",
-		"q1": "5",
-		"q2": "5",
-		"q3": "5",
-		"q4": "5",
-		"q5": "5"
+		"photo": "www.google.com",
+		"scores": [5, 5, 5, 5, 5]
 	},
 ];
 
