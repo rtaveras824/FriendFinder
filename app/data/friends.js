a120f03f0c34496e0f98cfe98b9dda4c6f83ad02
Reuben Taveras <rtaveras824@hotmail.com>
2016-09-24 23:29:51 -0400

    Changed design a bit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,24 @@
 var friends = [
 	{
-		"name": "Yuuu",
-		"photo": "www.google.com",
-		"scores": [1, 1, 1, 1, 1]
+		"name": "Iron Dude",
+		"photo": "http://dangsayz.com/images/profile-pics/profile-pics-10.jpg",
+		"scores": [1, 3, 2, 3, 1, 2, 4, 5, 3, 3]
 	},
 	{
-		"name": "Reuben",
-		"photo": "www.google.com",
-		"scores": [2, 2, 2, 2, 2]
+		"name": "Spider Dude",
+		"photo": "http://dangsayz.com/images/profile-pics/profile-pics-6.jpg",
+		"scores": [2, 2, 2, 2, 2, 2, 3, 4, 2, 1]
 	},
 	{
-		"name": "Babe",
-		"photo": "www.google.com",
-		"scores": [5, 5, 5, 5, 5]
+		"name": "Thor Dude",
+		"photo": "https://www.google.com/+/images/learnmore/hero/hero-profile.jpg",
+		"scores": [5, 5, 5, 5, 5, 3, 2, 4, 5, 3]
 	},
+	{
+		"name": "Captain Dude",
+		"photo": "https://assets.entrepreneur.com/content/16x9/822/20150406145944-dos-donts-taking-perfect-linkedin-profile-picture-selfie-mobile-camera-2.jpeg",
+		"scores": [2, 1, 1, 5, 3, 1, 2, 1, 3, 4]
+	}
 ];
 
 module.exports = friends;
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -4,7 +4,15 @@ module.exports = function(app) {
 
 	app.get('/', function(req, res) {
 		res.sendFile(path.join(__dirname, '../public/home.html'));
-	})
+	});
+
+	app.get('/index', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
+
+	app.get('/home', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
 
 	app.get('/survey', function(res, res) {
 		res.sendFile(path.join(__dirname, '../public/survey.html'));
